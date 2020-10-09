8a35a477cd7e80c55cc3b83053013cbe4791429c
Lydia Lundell <lgl29@ensign.edu>
2020-10-08 19:53:45 -0600

    hello

---
+++ other_scripts/LDSBC_IT249_ll_W1.5.sql
@@ -0,0 +1,60 @@
+--Q1: By Order Quantity, what were the five most popular products sold in 2014?
+
+--Include these data points in the output:
+
+--Order Date Year
+
+--Product ID
+
+--Product Name
+
+--Product Number
+
+--Product Color
+
+--Sales Order Count
+
+--Order Quantity
+
+--Sales Order Line total
+
+ 
+
+ 
+
+--Q2: How long are the 7 longest Person names and to whom do they belong? Rank by Full Name length, Last Name Length, First Name Length
+
+--Include these data points in the output:
+
+--Business Entity ID
+
+--Full Name
+
+--Full Name Length
+
+--First Name
+
+--First Name Length
+
+--Middle Name
+
+--Last Name
+
+--Last Name Length
+
+
+--Q3: Which Department pays its female workers on average the most per year?
+
+--Include these fields:
+
+--Department ID
+
+--Department Name
+
+--Gender
+
+--Total Yearly Pay
+
+--Business Entity ID Count
+
+--Average Yearly Pay
